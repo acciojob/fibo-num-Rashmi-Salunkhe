bea133cf9505a36a956819e0468db22d5663298f
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-24 11:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function isFibonacci(num) {
-		// Write your code here
-}
-const input = parseInt(prompt("Enter a number"));
-alert(isFibonacci(input));
+function isPerfectSquare(x) {
+			let s = Math.sqrt(x);
+			return s === Math.floor(s);
+		}
+
+		// Function to check if a number is a Fibonacci number
+		function isFibonacci(num) {
+			// Check if 5*num^2 + 4 or 5*num^2 - 4 is a perfect square
+			return isPerfectSquare(5 * num * num + 4) || isPerfectSquare(5 * num * num - 4);
+		}
+
+		// Take input from user and check if it's a Fibonacci number
+		const input = parseInt(prompt("Enter a number"));
+		alert(isFibonacci(input));
